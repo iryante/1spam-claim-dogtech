f8ed6210e5215fe970541dd69aa3727ab6190f22
iryante <iriantorahmat9875@gmail.com>
2023-11-15 15:03:44 +0000

    chore(bot): 👿 auto commit

--- Update.tsx
+++ Update.tsx
@@ -3970,3 +3970,7 @@ curl: (22) The requested URL returned error: 408
                                  Dload  Upload   Total   Spent    Left  Speed
   0     0    0     0    0     0      0      0 --:--:-- --:--:-- --:--:--     0100    60  100    60    0     0    200      0 --:--:-- --:--:-- --:--:--   201
 {"code":"InvalidParameters","error":"not found","data":null}
+  % Total    % Received % Xferd  Average Speed   Time    Time     Time  Current
+                                 Dload  Upload   Total   Spent    Left  Speed
+  0     0    0     0    0     0      0      0 --:--:-- --:--:-- --:--:--     0100    60  100    60    0     0    170      0 --:--:-- --:--:-- --:--:--   170
+{"code":"InvalidParameters","error":"not found","data":null}
